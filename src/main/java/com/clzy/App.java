0ba15ba6111171e83eb8a8eaec98b909919b6d16
baikun <baikun@clzytech.com>
2021-11-01 16:07:21 +0800

    新建的maven命令行项目

---
+++ src/main/java/com/clzy/App.java
@@ -0,0 +1,14 @@
+package com.clzy;
+
+/**
+ *
+ * @author baikun
+ * @date 2021/10/28 17:38
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
